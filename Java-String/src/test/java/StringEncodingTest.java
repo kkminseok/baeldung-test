3108e83ed6ceb3a3451f92466163a8ce2670a588
minseokkang <aaaa727@khu.ac.kr>
2025-01-30 03:34:51 +0900

    encoding test end

--- Java-String/src/test/java/StringEncodingTest.java
+++ Java-String/src/test/java/StringEncodingTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
-import java.nio.charset.Charset;
+import java.nio.charset.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -11,19 +11,59 @@ public class StringEncodingTest {
     @Test
     public void default_encoding_test() {
         try {
-            System.out.println(decodeText("The façade pattern is a software design pattern.", "US-ASCII"));
-        } catch (IOException e){
+            System.out.println(decodeText("The façade pattern is a software design pattern.", StandardCharsets.US_ASCII, CodingErrorAction.IGNORE));
+        } catch (IOException e) {
             System.out.println("error");
         }
     }
 
-    private String decodeText(String input, String encoding) throws IOException {
+    @Test
+    public void set_ignore_test() {
+        try {
+            Assertions.assertEquals(
+                    "The faade pattern is a software design pattern.",
+                    decodeText(
+                            "The façade pattern is a software design pattern.",
+                            StandardCharsets.US_ASCII,
+                            CodingErrorAction.IGNORE));
+        } catch (IOException e) {
+            System.out.println("error");
+        }
+    }
+
+    @Test
+    public void set_replace_test() {
+        try {
+            Assertions.assertEquals(
+                    "The fa��ade pattern is a software design pattern.",
+                    decodeText(
+                            "The façade pattern is a software design pattern.",
+                            StandardCharsets.US_ASCII,
+                            CodingErrorAction.REPLACE));
+        } catch (IOException e) {
+            System.out.println("error");
+        }
+    }
+
+    @Test
+    public void set_report_test() {
+        Assertions.assertThrows(
+                MalformedInputException.class,
+                () -> decodeText(
+                        "The façade pattern is a software design pattern.",
+                        StandardCharsets.US_ASCII,
+                        CodingErrorAction.REPORT));
+    }
+
+    private String decodeText(String input, Charset charset,
+                              CodingErrorAction codingErrorAction) throws IOException {
+        CharsetDecoder charsetDecoder = charset.newDecoder();
+        charsetDecoder.onMalformedInput(codingErrorAction);
         return
                 new BufferedReader(
                         new InputStreamReader(
                                 new ByteArrayInputStream(input.getBytes()),
-                                Charset.forName(encoding)))
-                        .readLine();
+                                charsetDecoder)).readLine();
     }
 
     @Test
