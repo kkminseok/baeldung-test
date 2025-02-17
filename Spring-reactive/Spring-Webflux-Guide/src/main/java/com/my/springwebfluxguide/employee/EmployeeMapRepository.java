36751b9cd1867ae3c1cf75414320988c366bf6fb
minseok-kang727 <minseok.kang727@nhn.com>
2025-02-17 09:24:55 +0900

    feature: Guide to Spring WebFlux

--- Spring-reactive/Spring-Webflux-Guide/src/main/java/com/my/springwebfluxguide/employee/EmployeeMapRepository.java
+++ Spring-reactive/Spring-Webflux-Guide/src/main/java/com/my/springwebfluxguide/employee/EmployeeMapRepository.java
@@ -16,4 +16,9 @@ public class EmployeeMapRepository implements EmployeeRepository{
     public Flux<Employee> getAllEmployees() {
         return Flux.just(new Employee(1, "minseok"), new Employee(2, "chill guy"));
     }
+
+    @Override
+    public Mono<Employee> updateEmployee(Employee employee) {
+        return Mono.just(new Employee(1, "minseok updated"));
+    }
 }
