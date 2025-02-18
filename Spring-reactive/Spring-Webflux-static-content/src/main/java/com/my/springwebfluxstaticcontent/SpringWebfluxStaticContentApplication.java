16ed9fe2253758ae03c5635b099a0b2cdfbc42b6
minseok-kang727 <minseok.kang727@nhn.com>
2025-02-18 15:21:29 +0900

    feature: Static Content in Spring WebFlux

---
+++ Spring-reactive/Spring-Webflux-static-content/src/main/java/com/my/springwebfluxstaticcontent/SpringWebfluxStaticContentApplication.java
@@ -0,0 +1,13 @@
+package com.my.springwebfluxstaticcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxStaticContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxStaticContentApplication.class, args);
+    }
+
+}
---
+++ Spring-reactive/Spring-Webflux-static-content/src/test/java/com/my/springwebfluxstaticcontent/SpringWebfluxStaticContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.springwebfluxstaticcontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxStaticContentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
