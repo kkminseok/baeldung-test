488f202707affb43048df1823bd5ff415f3e4a34
minseok-kang727 <minseok.kang727@nhn.com>
2025-04-07 09:46:17 +0900

    fix: 최신화정보 추가

--- My-test/Spring-Webflux-Mono-Test/src/test/java/com/my/springwebfluxmonotest/MonoTest.java
+++ My-test/Spring-Webflux-Mono-Test/src/test/java/com/my/springwebfluxmonotest/MonoTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.UUID;
+
 public class MonoTest {
 
     @Test
@@ -37,6 +39,28 @@ public class MonoTest {
     }
 
     @Test
+    void MonoDefer_UUID_test() {
+        Mono<String> defer = Mono.defer(() -> {
+            System.out.println("Mono defer");
+            return Mono.just(UUID.randomUUID().toString());
+        });
+
+        defer.subscribe(System.out::println);
+        defer.subscribe(System.out::println);
+    }
+
+    @Test
+    void MonoFromCallable_UUID_test() {
+        Mono<String> callable = Mono.fromCallable(() -> {
+            System.out.println("Callable 실행");
+            return UUID.randomUUID().toString();
+        });
+
+        callable.subscribe(System.out::println);
+        callable.subscribe(System.out::println);
+    }
+
+    @Test
     void testMonoFromSupplier() {
         Mono<String> mono = Mono.fromSupplier(() -> {
             System.out.println("Generating Value");
@@ -49,4 +73,7 @@ public class MonoTest {
                 .expectNext("Hello")
                 .verifyComplete();
     }
+
+
+
 }
