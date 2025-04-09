2950d6390d6a222b58560721e2e049ea6917dae3
minseok-kang727 <minseok.kang727@nhn.com>
2025-04-09 09:31:12 +0900

    [Spring-messaging] sample3 fix

--- Spring-messaging/Spring-Boot-Kafka-Batch-Sample/src/main/java/com/my/springbootkafkabatchsample/SpringBootKafkaBatchSampleApplication.java
+++ Spring-messaging/Spring-Boot-Kafka-Batch-Sample/src/main/java/com/my/springbootkafkabatchsample/SpringBootKafkaBatchSampleApplication.java
@@ -4,32 +4,46 @@ import com.my.springbootkafkabatchsample.model.Foo2;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.ProducerListener;
 import org.springframework.kafka.support.converter.BatchMessagingMessageConverter;
 import org.springframework.kafka.support.converter.JsonMessageConverter;
 import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 public class SpringBootKafkaBatchSampleApplication {
 
+
     final static CountDownLatch LATCH = new CountDownLatch(1);
 
     public static void main(String[] args) throws InterruptedException {
-        ConfigurableApplicationContext run = SpringApplication.run(SpringBootKafkaBatchSampleApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootKafkaBatchSampleApplication.class, args);
         LATCH.await();
         Thread.sleep(5_000);
-        run.close();
+        context.close();
     }
 
     @Bean
@@ -38,8 +52,14 @@ public class SpringBootKafkaBatchSampleApplication {
     }
 
     @Bean
-    public BatchMessagingMessageConverter batchConvert() {
+    public BatchMessagingMessageConverter batchConverter() {
         return new BatchMessagingMessageConverter(converter());
+    }
+
+
+    @Bean
+    public NewTopic topic1() {
+        return TopicBuilder.name("topic1").partitions(1).replicas(1).build();
     }
 
     @Bean
@@ -61,17 +81,22 @@ class Listener {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    @KafkaListener(id = "fooGroup1", topics = "topic1")
+    public void listenFoo1(Foo2 record) {
+        log.info("Foo1: {}", record);
+    }
+
     @KafkaListener(id = "fooGroup2", topics = "topic2")
     public void listen1(List<Foo2> foos) throws IOException {
-        log.info("Received : {}", foos);
+        log.info("Received: " + foos);
         foos.forEach(f -> kafkaTemplate.send("topic3", f.foo().toUpperCase()));
         log.info("Messages sent, hit Enter to commit tx");
-        //System.in.read();
+        System.in.read();
     }
 
     @KafkaListener(id = "fooGroup3", topics = "topic3")
     public void listen2(List<String> in) {
-        log.info("Received : {}", in);
+        log.info("Received: " + in);
         SpringBootKafkaBatchSampleApplication.LATCH.countDown();
     }
 
--- Spring-messaging/Spring-Boot-Kafka-Batch-Sample/src/main/java/com/my/springbootkafkabatchsample/controller/Controller.java
+++ Spring-messaging/Spring-Boot-Kafka-Batch-Sample/src/main/java/com/my/springbootkafkabatchsample/controller/Controller.java
@@ -5,6 +5,7 @@ import com.my.springbootkafkabatchsample.model.Foo1;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,12 +21,19 @@ public class Controller {
     @PostMapping("/send/foos/{what}")
     public void sendFoo(@PathVariable String what) {
         log.info("sample3");
+        //template.send("topic1", new Foo1(what));
+
         template.executeInTransaction(template -> {
+            log.info("hello");
             StringUtils.commaDelimitedListToSet(what).stream()
                     .map(s -> new Foo1(s))
                     .forEach(foo -> {
-                        log.info("data: {}", foo);
-                        template.send("topic2", foo);
+                        try {
+                            log.info("data: {}", foo);
+                            template.send("topic2", foo);
+                        } catch (Exception e) {
+                            log.error("error: {}", e);
+                        }
                     });
             return null;
         });
