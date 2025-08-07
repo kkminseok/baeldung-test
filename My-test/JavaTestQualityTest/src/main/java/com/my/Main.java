f74c361b10e19ac1be0ebeaccb14f7ca04b3d14b
kkminseok <aaaa727@khu.ac.kr>
2025-08-07 19:30:45 +0900

    [Java-Test] 문자열 관련 실수할만한 내용 정리

--- My-test/JavaTestQualityTest/src/main/java/com/my/Main.java
+++ My-test/JavaTestQualityTest/src/main/java/com/my/Main.java
@@ -1,7 +1,12 @@
 package com.my;
 
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        Arrays.stream("Hello.Java Test Quality.Test!".split(".")).toList().forEach(System.out::println);
     }
 }
--- My-test/JavaTestQualityTest/src/test/java/QualityTest.java
+++ My-test/JavaTestQualityTest/src/test/java/QualityTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -29,5 +30,62 @@ public class QualityTest {
         list.sort(Integer::max);
         // [0, -3 , 3, -1, 1, 2]
         System.out.println(list);
+    }
+
+    @Test
+    @DisplayName("정수 나눗셈 중 반올림 문제")
+    void testIntegerDivisionRoundingIssue() {
+        Assertions.assertEquals(process(2), process(3));
+    }
+
+    private double process(int value) {
+        double half = value / 2;
+        return half;
+    }
+
+    @Test
+    @DisplayName("Integer Min Value특수성")
+    void testIntegerMinValueSpecialCase() {
+        Assertions.assertEquals(Math.abs(Integer.MIN_VALUE), Integer.MIN_VALUE);
+    }
+
+    @Test
+    @DisplayName("Double 최솟값 확인")
+    void testDoubleMinValue() {
+        System.out.println("Double.MIN_VALUE: " + Double.MIN_VALUE);
+        System.out.println("-Double.MAX_VALUE" + (-Double.MAX_VALUE));
+        Assertions.assertEquals(Double.MIN_VALUE, 4.9E-324);
+    }
+
+    @Test
+    @DisplayName("replaceAll 메서드 실수 테스트")
+    void testReplaceAllMethodMistake() {
+        String input = "$0minseok";
+        String template = "Hello USER_NAME";
+        String result = template.replaceAll("USER_NAME", input);
+        Assertions.assertEquals("Hello USER_NAMEminseok", result);
+    }
+
+    @Test
+    @DisplayName("indexOf 메서드 실수 테스트")
+    void testIndexOfMethodMistake() {
+        String input = """
+                ]AAAAAAAAAA
+                AAAAAAAAAA
+                AAAAAAAAAA
+                AAAAAAAAAA
+                AAAAAAAAAA
+                AAAAAAAAAA
+                AAAAAAAAAA
+                AAAAAAAAAA
+                AAAAAAAAAA
+                AAAAAAAAAA
+                AAAAAAA[AA
+                v
+                """;
+        int start = input.indexOf('[');
+        int end = input.indexOf(start, ']');
+        Assertions.assertEquals(118, start);
+        Assertions.assertEquals(122,end);
     }
 }
